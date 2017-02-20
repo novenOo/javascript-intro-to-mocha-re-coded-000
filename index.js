f790eefe3f73b9a147a807b0aab7ac705bc2903f
Noven Temer <noven5823@gmail.com>
2017-02-20 20:53:19 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth(){
+	return true;
+}
+function sayHey(){
+    return "Hey!"	
+}
+function sayHeyFriend(name) {
+	return `Hey, ${name}!`
+}
+function favoriteIceCream(iceCream){
+	return `I love ${iceCream}`
+}
+function shouting(upper){
+	return upper.toUpperCase();
+}
+function roundDown(number){
+    return Math.floor(number);
+}
